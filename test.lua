c22afa3a5d075a872584794a97f0c94d1002221c
Sergey Zagoruyko <sergey.zagoruyko@imagine.enpc.fr>
2014-11-10 20:59:17 +0100

    rename

--- test.lua
+++ test.lua
@@ -4,7 +4,7 @@ dofile 'SpatialAdaptiveMaxPooling.lua'
 
 input = torch.Tensor(32, 3, 58, 40):cuda()
 
-module = nn.SpatialAdaptivePooling(4,4):cuda()
+module = nn.SpatialAdaptiveMaxPooling(4,4):cuda()
 
 print(module:forward(input):size())
 

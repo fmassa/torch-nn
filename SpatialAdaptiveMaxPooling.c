f29ba91a2cadd1957593354203901b55eba3dda3
fsuzanomassa <fvsmassa@gmail.com>
2014-11-10 21:14:50 +0100

    Hacks to merge with Sergey

---
+++ SpatialAdaptiveMaxPooling.c
@@ -0,0 +1,65 @@
+#define real float
+#define FLT_MAX 3.40282347E+38F
+
+void SpatialAdaptiveMaxPooling_updateOutput_frame(real *input_p,
+                                                      real *output_p,
+                                                      //real *indx_p, real *indy_p,
+                                                      long nslices,
+                                                      long iwidth, long iheight,
+                                                      long owidth, long oheight)
+{
+  long k;
+#pragma omp parallel for private(k)
+  for (k = 0; k < nslices; k++)
+  {
+    /* loop over output */
+    long i, j;
+    for(i = 0; i < oheight; i++)
+    {
+      int y_start = (int)floor((real)i / oheight * iheight);
+      int y_end   = (int)ceil((real)(i + 1) / oheight * iheight);
+      int kH = y_end-y_start;
+      
+      for(j = 0; j < owidth; j++)
+      {
+        
+        int x_start = (int)floor((real)j / owidth * iwidth);
+        int x_end   = (int)ceil((real)(j + 1) / owidth * iwidth);
+        int kW = x_end-x_start;
+        
+        /* local pointers */
+        real *ip = input_p   + k*iwidth*iheight + y_start*iwidth + x_start;
+        real *op = output_p  + k*owidth*oheight + i*owidth + j;
+        //real *indyp = indy_p + k*owidth*oheight + i*owidth + j;
+        //real *indxp = indx_p + k*owidth*oheight + i*owidth + j;
+
+        /* compute local max: */
+        long maxindex = -1;
+        real maxval = -FLT_MAX;
+        long tcntr = 0;
+        int x,y;
+        for(y = 0; y < kH; y++)
+        {
+          for(x = 0; x < kW; x++)
+          {
+            real val = *(ip + y*iwidth + x);
+            if (val > maxval)
+            {
+              maxval = val;
+              maxindex = tcntr;
+            }
+            tcntr++;
+          }
+        }
+
+        /* set output to local max */
+        *op = maxval;
+
+        /* store location of max (x,y) */
+        //*indyp = (int)(maxindex / kW)+1;
+        //*indxp = (maxindex % kW) +1;
+      }
+    }
+  }
+}
+
---
+++ SpatialAdaptiveMaxPoolingFloat.lua
@@ -0,0 +1,57 @@
+require 'nn'
+
+local ffi = require 'ffi'
+
+ffi.cdef[[
+void SpatialAdaptiveMaxPooling_updateOutput_frame(float *input_p,
+                                                      float *output_p,
+                                                      //real *indx_p, real *indy_p,
+                                                      long nslices,
+                                                      long iwidth, long iheight,
+                                                      long owidth, long oheight);
+
+]]
+
+local C = ffi.load 'libadapt.so'
+
+local SpatialAdaptiveMaxPooling, parent = torch.class('nn.SpatialAdaptiveMaxPooling', 'nn.Module')
+
+function SpatialAdaptiveMaxPooling:__init(W, H)
+  parent.__init(self)
+  
+  self.W = W
+  self.H = H or W
+  
+  self.output = torch.Tensor()
+  self.gradInput = torch.Tensor()
+  
+  self.indices = torch.Tensor()
+end
+
+function SpatialAdaptiveMaxPooling:updateOutput(input)
+  local dimw = 3
+  local dimh = 2
+  local nslices = input:size(1)
+  if input:nDimension() == 4 then 
+    dimw = dimw + 1
+    dimh = dimh + 1
+    self.output:resize(input:size(1),input:size(2),self.H,self.W)
+    nslices = nslices*input:size(2)
+  else
+    self.output:resize(input:size(1),self.H,self.W)
+  end
+  
+  
+  assert(input:size(dimw)>=self.W, "input width should be greater or equal than W")
+  assert(input:size(dimh)>=self.H, "input height should be greater or equal than H")
+  
+ 
+  C['SpatialAdaptiveMaxPooling_updateOutput_frame'](input:data(), self.output:data(), nslices, input:size(dimw), input:size(dimh), self.W, self.H)  
+  
+  return self.output
+
+end
+
+function SpatialAdaptiveMaxPooling:updateGradInput(input, gradOutput)
+  error('Not yet implemented')
+end
